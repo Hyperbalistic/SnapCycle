f3e342dec908bff09d4d7f59f90848f5547e90ba
Hyperbalistic <148830554+Hyperbalistic@users.noreply.github.com>
2025-06-26 21:23:33 -0400

    Add files via upload

---
+++ How It Works.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './HowItWorks.css';
+
+export default function HowItWorks() {
+    return (
+        <div className="container">
+            <div className="decorative-circles">
+                <div className="circle circle-1"></div>
+                <div className="circle circle-2"></div>
+                <div className="circle circle-3"></div>
+            </div>
+            
+            <h1>How It Works</h1>
+            <p>Place Holder</p>
+            
+            <div className="box">
+                <h2>Step 1</h2>
+                <p>Place Holder</p>
+            </div>
+
+            <div className="box">
+                <h2>Step 2</h2>
+                <p>Place Holder</p>
+            </div>
+
+            <div className="box">
+                <h2>Step 3</h2>
+                <p>Place Holder</p>
+            </div>
+
+            <div className="box">
+                <h2>Step 4</h2>
+                <p>Place Holder</p>
+            </div>
+
+            <div className="box">
+                <h2>Step 5</h2>
+                <p>Place Holder</p>
+            </div>
+        </div>
+    );
+} 
